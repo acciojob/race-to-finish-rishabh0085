87023059894e204c708d8d0c0e5c649177267fa0
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2023-11-28 23:17:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+// script.js
+const promises = Array.from({ length: 5 }, (_, index) => {
+  // Create promises that resolve after a random time between 1 and 5 seconds
+  return new Promise(resolve => {
+    const randomTime = Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
+    setTimeout(() => {
+      resolve(`Promise ${index + 1} resolved in ${randomTime / 1000} seconds`);
+    }, randomTime);
+  });
+});
+
+Promise.any(promises)
+  .then(result => {
+    // Print the result to the output div
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = result;
+  })
+  .catch(error => {
+    // Handle errors if all promises are rejected
+    console.error('All promises were rejected:', error);
+  });
